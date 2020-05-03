6242a5603c206484c2fc83be238b722285a399ee
MVONDO Yannick <mvondoyannick@gmail.com>
2020-05-03 23:37:39 +0100

    update remove cycle #2

--- db/migrate/20200408065305_remove_structure_from_cycle.rb
+++ db/migrate/20200408065305_remove_structure_from_cycle.rb
@@ -1,5 +1,5 @@
 class RemoveStructureFromCycle < ActiveRecord::Migration[6.0]
   def change
-    remove_reference :cycles, :structure, null: false, foreign_key: true
+    # remove_reference :cycles, :structure, null: false, foreign_key: true
   end
 end
