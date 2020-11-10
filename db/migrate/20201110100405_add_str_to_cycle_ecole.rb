3ad4eb5d484f50d9e673056948f3ba4ecf89dc82
MVONDO Yannick <mvondoyannick@gmail.com>
2020-11-10 16:41:16 +0100

    update some feature #2

--- db/migrate/20201110100405_add_str_to_cycle_ecole.rb
+++ db/migrate/20201110100405_add_str_to_cycle_ecole.rb
@@ -1,5 +1,5 @@
 class AddStrToCycleEcole < ActiveRecord::Migration[6.0]
   def change
-    add_reference :cycle_ecoles, :structure, null: true, foreign_key: true
+    # add_reference :cycle_ecoles, :structure, null: true, foreign_key: true
   end
 end
