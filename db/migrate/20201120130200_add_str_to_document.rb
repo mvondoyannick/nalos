bbd15ebaeba40cebaa5287cdf29ae0e30e4f7d35
MVONDO Yannick <mvondoyannick@gmail.com>
2020-11-20 14:08:34 +0100

    update document-> add document #3

--- db/migrate/20201120130200_add_str_to_document.rb
+++ db/migrate/20201120130200_add_str_to_document.rb
@@ -1,5 +1,5 @@
 class AddStrToDocument < ActiveRecord::Migration[6.0]
   def change
-    add_reference :documents, :structure, null: false, foreign_key: true
+    add_reference :documents, :structure, null: true, foreign_key: true
   end
 end
